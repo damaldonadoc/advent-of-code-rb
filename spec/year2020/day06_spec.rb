daf356688f9e103a51fcf859941b1b88d997744c
Diego Maldonado <damaldonadoc@protonmail.com>
2020-12-06 14:23:09 +0100

    add solution for day 6 - part 2

--- spec/year2020/day06_spec.rb
+++ spec/year2020/day06_spec.rb
@@ -28,6 +28,24 @@ RSpec.describe Year2020::Day06 do
 
   it 'solves part2' do
     d = Year2020::Day06.new
-    expect(d.part2('some_input')).to eq(nil)
+    input = <<~TXT
+      abc
+
+      a
+      b
+      c
+
+      ab
+      ac
+
+      a
+      a
+      a
+      a
+
+      b
+    TXT
+
+    expect(d.part2(input)).to eq(6)
   end
 end
--- src/year2020/day06.rb
+++ src/year2020/day06.rb
@@ -2,20 +2,30 @@
 
 module Year2020
   class Day06
+    QUESTION_LIST = ('a'..'z').freeze
+
     def part1(input)
       grouped_answers(input).sum do |group|
         group.chars.uniq.count
       end
     end
 
-    def part2(_input)
-      nil
+    def part2(input)
+      group_answers_by_person(input).sum do |group|
+        group.reduce(QUESTION_LIST.to_a) do |acc, answer|
+          acc & answer.chars
+        end.size
+      end
     end
 
     private
 
     def grouped_answers(input)
       input.split("\n\n").map { |g| g.delete("\n") }
+    end
+
+    def group_answers_by_person(input)
+      input.split("\n\n").map(&:split)
     end
   end
 end
