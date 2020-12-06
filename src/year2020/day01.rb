850ce233181b8220ab640a9e17b5cef1941217a6
Diego Maldonado <damaldonadoc@protonmail.com>
2020-12-06 14:57:26 +0100

    update solution for day 1, now using Array#combination instead of each loops

--- src/year2020/day01.rb
+++ src/year2020/day01.rb
@@ -3,25 +3,14 @@
 module Year2020
   class Day01
     def part1(input)
-      input.lines.reduce do |_acc, line|
-        pair = input.lines.find { |x| line.to_i + x.to_i == 2020 }
-
-        break line.to_i * pair.to_i if pair
+      input.lines.map(&:to_i).combination(2) do |comb|
+        return comb[0] * comb[1] if comb[0] + comb[1] == 2020
       end
     end
 
     def part2(input)
-      input.lines.each_with_index do |line, x|
-        rest_of_lines = input.lines.drop(x + 1)
-        break if rest_of_lines.size < 2
-
-        rest_of_lines.each_with_index do |second_line, y|
-          third_line = rest_of_lines.drop(y + 1).find do |temp_line|
-            line.to_i + second_line.to_i + temp_line.to_i == 2020
-          end
-
-          return line.to_i * second_line.to_i * third_line.to_i if third_line
-        end
+      input.lines.map(&:to_i).combination(3) do |comb|
+        return comb[0] * comb[1] * comb[2] if comb[0] + comb[1] + comb[2] == 2020
       end
     end
   end
