3b7529a7d1eafd3d7f6ef01b9a1c9ad93f5e045c
Diego Maldonado <damaldonadoc@protonmail.com>
2020-12-12 07:32:14 +0100

    add solution for day 10 - part 2

--- spec/year2020/day10_spec.rb
+++ spec/year2020/day10_spec.rb
@@ -44,6 +44,40 @@ RSpec.describe Year2020::Day10 do
 
   it 'solves part2' do
     d = Year2020::Day10.new
-    expect(d.part2('some input')).to eq(nil)
+    input = <<~TXT
+      28
+      33
+      18
+      42
+      31
+      14
+      46
+      20
+      48
+      47
+      24
+      23
+      49
+      45
+      19
+      38
+      39
+      11
+      1
+      32
+      25
+      35
+      8
+      17
+      7
+      9
+      4
+      2
+      34
+      10
+      3
+    TXT
+
+    expect(d.part2(input)).to eq(19208)
   end
 end
--- src/year2020/day10.rb
+++ src/year2020/day10.rb
@@ -15,8 +15,23 @@ module Year2020
       diff_count[:'1'] * diff_count[:'3']
     end
 
-    def part2(_input)
-      nil
+    def part2(input)
+      rates = input.split("\n").map(&:to_i)
+      device_rate = rates.max + 3
+
+      rates = rates.push(device_rate).sort
+
+      # create a Hash with rates as keys, '0' has 1 as value,
+      # the rest of the keys has 0 as value
+      rates_hash = Hash[[[0, 1]] + rates.map { |r| [r, 0] }]
+
+      rates_hash.each do |rate, combinations|
+        ((rate + 1)..(rate + 3)).each do |x|
+          rates_hash[x] += combinations if rates_hash[x]
+        end
+      end
+
+      rates_hash[device_rate]
     end
   end
 end
