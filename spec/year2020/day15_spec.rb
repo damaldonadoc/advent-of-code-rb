c18f3fc7338ffe63dd12fb9af200ecc3cebb8015
Diego Maldonado <damaldonadoc@protonmail.com>
2020-12-18 07:42:29 +0100

    add solution for day 15 - part 1

---
+++ spec/year2020/day15_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Year2020::Day15 do
+  it 'solves part1' do
+    d = Year2020::Day15.new
+    input = '0,3,6'
+
+    expect(d.part1(input)).to eq(436)
+  end
+
+  it 'solves part2' do
+    d = Year2020::Day15.new
+    expect(d.part2('some_input')).to eq(nil)
+  end
+end
---
+++ src/year2020/day15.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module Year2020
+  class Day15
+    def part1(input)
+      numbers = input.split(',').map(&:to_i)
+      initial_history = Hash[
+        numbers.map.with_index { |n, index| [n, [index + 1]] }
+      ]
+
+      play_game(
+        initial_history,
+        numbers.last,
+        numbers.size + 1,
+        2020
+      ).last
+    end
+
+    def part2(_input)
+      nil
+    end
+
+    private
+
+    def play_game(history, last, turn, max_turns)
+      return [history, last] if turn > max_turns
+
+      current = if history[last].size < 2
+                  0
+                else
+                  history[last].last(2).reduce(:-).abs
+                end
+
+      history[current] = history[current] ? history[current] << turn : [turn]
+
+      play_game(history, current, turn + 1, max_turns)
+    end
+  end
+end
