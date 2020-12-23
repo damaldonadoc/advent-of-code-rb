a783d0a9e1fce4ce680b107646eb332932d0f24d
Diego Maldonado <damaldonadoc@protonmail.com>
2020-12-23 07:40:43 +0100

    add solution for day 19 - part 2

--- src/year2020/day19.rb
+++ src/year2020/day19.rb
@@ -2,6 +2,8 @@
 
 module Year2020
   class Day19
+    STACK_LIMIT = 10
+
     def part1(input)
       rules_str, messages = input.split("\n\n")
       rules = format_rules(rules_str)
@@ -10,8 +12,16 @@ module Year2020
       messages.scan(/^#{rule_exp}$/).count
     end
 
-    def part2(_input)
-      nil
+    def part2(input)
+      rules_str, messages = input.split("\n\n")
+
+      rules_str.gsub!('8: 42', '8: 42 | 42 8')
+      rules_str.gsub!('11: 42 31', '11: 42 31 | 42 11 31')
+
+      rules = format_rules(rules_str)
+      rule_exp = rule_expression(rules, '0', {})
+
+      messages.scan(/^#{rule_exp}$/).count
     end
 
     private
@@ -31,13 +41,24 @@ module Year2020
       end
     end
 
-    def rule_expression(rules, key)
+    def rule_expression(rules, key, prev_calls = {})
       rule = rules[key]
       return key if rule.nil?
       return rule if rule.is_a?(String)
 
       rule.map do |rule_key|
-        rule_expression(rules, rule_key)
+        prev_calls[rule_key] ||= 1
+        prev_calls[rule_key] += 1
+
+        # return the rule expression without the element that causes the loop
+        # when reaching the recursive stack limit
+        if key == rule_key && prev_calls[rule_key] == STACK_LIMIT
+          rule.filter { |el| el.match?(/\d+/) && el != rule_key }.map do |k|
+            rule_expression(rules, k, prev_calls)
+          end.join
+        else
+          rule_expression(rules, rule_key, prev_calls)
+        end
       end.join
     end
   end
