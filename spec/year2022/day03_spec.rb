97a2232c8b8f6f81302f14547a6ce36fd32c616b
Diego Maldonado <damaldonadoc@protonmail.com>
2022-12-04 00:54:10 +0100

    add solution for day 03 - part 2 - 2022

--- spec/year2022/day03_spec.rb
+++ spec/year2022/day03_spec.rb
@@ -19,6 +19,15 @@ RSpec.describe Year2022::Day03 do
 
   it 'solves part2' do
     d = Year2022::Day03.new
-    expect(d.part2('some_input')).to eq(nil)
+    input = <<~TXT
+      vJrwpWtwJgWrhcsFMMfFFhFp
+      jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+      PmmdzqPrVvPwwTWBwg
+      wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+      ttgJtRGJQctTZtZT
+      CrZsJsPPZsGzwwsLwLmpwMDw
+    TXT
+
+    expect(d.part2(input)).to eq(70)
   end
 end
--- src/year2022/day03.rb
+++ src/year2022/day03.rb
@@ -3,7 +3,7 @@
 module Year2022
   class Day03
     def part1(input)
-      input.lines.reduce(0) do |acc,rucksack|
+      input.lines.reduce(0) do |acc, rucksack|
         items_c1, items_c2 = rucksack.chars.each_slice(rucksack.size / 2).to_a
 
         duplicated_item = (items_c1 & items_c2).first
@@ -14,8 +14,14 @@ module Year2022
       end
     end
 
-    def part2(_input)
-      nil
+    def part2(input)
+      input.lines.each_slice(3).reduce(0) do |acc, rucksack_group|
+        badge_item = rucksack_group.map(&:chars).inject(:&).first
+
+        offset = badge_item.ord > 90 ? 96 : 38
+
+        acc + (badge_item.ord - offset)
+      end
     end
   end
 end
