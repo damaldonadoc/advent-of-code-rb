c2139d1ae4b2c7a0919d010f47892a57b95f030d
Diego Maldonado <damaldonadoc@protonmail.com>
2022-12-09 23:40:30 +0100

    add solution for day 08 - part 2 - 2022

--- spec/year2022/day08_spec.rb
+++ spec/year2022/day08_spec.rb
@@ -18,6 +18,14 @@ RSpec.describe Year2022::Day08 do
 
   it 'solves part2' do
     d = Year2022::Day08.new
-    expect(d.part2('some_input')).to eq(nil)
+    input = <<~TXT
+      30373
+      25512
+      65332
+      33549
+      35390
+    TXT
+
+    expect(d.part2(input)).to eq(8)
   end
 end
--- src/year2022/day08.rb
+++ src/year2022/day08.rb
@@ -19,8 +19,17 @@ module Year2022
       visible_trees
     end
 
-    def part2(_input)
-      nil
+    def part2(input)
+      grid = input.lines.map { |line| line.strip.chars.map(&:to_i) }
+
+      width = grid.first.count
+      height = grid.count
+
+      (1...(height - 1)).map do |y|
+        (1...(width - 1)).map do |x|
+          scenic_score(grid, width, height, x, y)
+        end
+      end.flatten.max
     end
 
     private
@@ -34,6 +43,37 @@ module Year2022
         ((y + 1)...height).none? { |delta_y| target_tree <= grid[delta_y][x] } ||
         (0..(x - 1)).none? { |delta_x| target_tree <= grid[y][delta_x] } ||
         ((x + 1)...width).none? { |delta_x| target_tree <= grid[y][delta_x] }
+    end
+
+    def scenic_score(grid, height, width, *coordinates)
+      x, y = coordinates
+      target_tree = grid[y][x]
+
+      viewing_distance_up = (0...y).to_a.reverse.reduce(0) do |acc, delta_y|
+        break acc + 1 if target_tree <= grid[delta_y][x]
+
+        acc + 1
+      end
+
+      viewing_distance_down = ((y + 1)...height).reduce(0) do |acc, delta_y|
+        break acc + 1 if target_tree <= grid[delta_y][x]
+
+        acc + 1
+      end
+
+      viewing_distance_left = (0...x).to_a.reverse.reduce(0) do |acc, delta_x|
+        break acc + 1 if target_tree <= grid[y][delta_x]
+
+        acc + 1
+      end
+
+      viewing_distance_right = ((x + 1)...width).reduce(0) do |acc, delta_x|
+        break acc + 1 if target_tree <= grid[y][delta_x]
+
+        acc + 1
+      end
+
+      viewing_distance_up * viewing_distance_down * viewing_distance_left * viewing_distance_right
     end
   end
 end
