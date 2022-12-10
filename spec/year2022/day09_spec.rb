40c1d1cb5aed1537842fc1432d50cbd0348c5ee9
Diego Maldonado <damaldonadoc@protonmail.com>
2022-12-10 23:54:55 +0100

    add solution for day 09 - part 2 - 2022

--- spec/year2022/day09_spec.rb
+++ spec/year2022/day09_spec.rb
@@ -21,6 +21,30 @@ RSpec.describe Year2022::Day09 do
 
   it 'solves part2' do
     d = Year2022::Day09.new
-    expect(d.part2('some_input')).to eq(nil)
+    input = <<~TXT
+      R 4
+      U 4
+      L 3
+      D 1
+      R 4
+      D 1
+      L 5
+      R 2
+    TXT
+
+    expect(d.part2(input)).to eq(1)
+
+    input = <<~TXT
+      R 5
+      U 8
+      L 8
+      D 3
+      R 17
+      D 10
+      L 25
+      U 20
+    TXT
+
+    expect(d.part2(input)).to eq(36)
   end
 end
--- src/year2022/day09.rb
+++ src/year2022/day09.rb
@@ -10,46 +10,48 @@ module Year2022
     }
 
     def part1(input)
-      rope_state = {
-        head_position: [0, 0],
-        tail_position: [0, 0]
-      }
+      rope_state = [[0, 0]] * 2
 
       input.lines.map do |line|
         direction, moves = *line.split
 
         moves.to_i.times.map do
-          move_rope!(direction, rope_state)[:tail_position]
+          move_rope!(direction, rope_state).last
         end
       end.flatten(1).uniq.count
     end
 
-    def part2(_input)
-      nil
+    def part2(input)
+      rope_state = [[0, 0]] * 10
+
+      input.lines.map do |line|
+        direction, moves = *line.split
+
+        moves.to_i.times.map do
+          move_rope!(direction, rope_state).last
+        end
+      end.flatten(1).uniq.count
     end
 
     private
 
     def move_rope!(direction, rope_state)
-      new_head_position = [DIRECTIONS[direction], rope_state[:head_position]].transpose.map(&:sum)
+      rope_state[0] = [DIRECTIONS[direction], rope_state[0]].transpose.map(&:sum)
 
-      adjacent_pocitions = [-1, 0, 1].repeated_permutation(2).to_a.map do |d|
-        [new_head_position, d].transpose.map(&:sum)
+      # update position of each knot based on head movement
+      (0...(rope_state.size - 1)).each do |i|
+        leading_knot = rope_state[i]
+        trailing_knot = rope_state[i + 1]
+
+        difference = [leading_knot, trailing_knot].transpose.map { |x| x.reduce(:-) }
+        distance = difference.map(&:abs).sum.to_f
+
+        next if distance.zero?
+
+        delta = difference.map { |x| (x / distance).round }
+
+        rope_state[i + 1] = [leading_knot, delta].transpose.map { |x| x.reduce(:-) }
       end
-
-      unless adjacent_pocitions.include?(rope_state[:tail_position])
-        rope_state[:tail_position] =
-          if new_head_position[0] == rope_state[:tail_position][0] ||
-             new_head_position[1] == rope_state[:tail_position][1]
-
-            [DIRECTIONS[direction], rope_state[:tail_position]].transpose.map(&:sum)
-          else
-            rope_state[:head_position]
-          end
-
-      end
-
-      rope_state[:head_position] = new_head_position
 
       rope_state
     end
