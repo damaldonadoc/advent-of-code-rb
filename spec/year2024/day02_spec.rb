4a5e81d065b3a10819d23569a5978929a5512bb1
Diego Maldonado <diego.maldonado@lingokids.com>
2024-12-04 18:00:15 +0100

    add solution for day 2 - part 2 - 2024

--- spec/year2024/day02_spec.rb
+++ spec/year2024/day02_spec.rb
@@ -17,6 +17,15 @@ RSpec.describe Year2024::Day02 do
 
   it 'solves part2' do
     d = Year2024::Day02.new
-    expect(d.part2('some_input')).to eq(nil)
+    input = <<~TXT
+      7 6 4 2 1
+      1 2 7 8 9
+      9 7 6 2 1
+      1 3 2 4 5
+      8 6 4 4 1
+      1 3 6 7 9
+    TXT
+
+    expect(d.part2(input)).to eq(4)
   end
 end
--- src/year2024/day02.rb
+++ src/year2024/day02.rb
@@ -1,21 +1,34 @@
 # frozen_string_literal: true
 
-#
 module Year2024
   class Day02
     def part1(input)
       reports = input.lines.map { |line| line.split.map(&:to_i) }
 
       reports.count do |levels|
-        differences = levels.each_cons(2).map { |a, b| b - a }
-        next false unless differences.all?(&:positive?) || differences.all?(&:negative?)
-
-        differences.all? { |d| d.abs.between?(1, 3) }
+        safe_report?(levels)
       end
     end
 
     def part2(input)
-      nil
+      reports = input.lines.map { |line| line.split.map(&:to_i) }
+
+      reports.count do |levels|
+        safe_report?(levels) || levels.each_index.any? do |i|
+          safe_report?(levels.reject.with_index do |_, idx|
+            idx == i
+          end)
+        end
+      end
+    end
+
+    private
+
+    def safe_report?(levels)
+      differences = levels.each_cons(2).map { |a, b| b - a }
+
+      differences.all? { |d| d.abs.between?(1, 3) } &&
+        (differences.all?(&:positive?) || differences.all?(&:negative?))
     end
   end
 end
